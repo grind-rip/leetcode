8d3cd2cba48b42d6730fe59123809e5a603f0bcc
nickolashkraus <0x@nickolaskraus.io>
2025-01-10 09:08:46 -0600

    Add `TreeNode`

--- src/classes/__init__.py
+++ src/classes/__init__.py
@@ -8,3 +8,11 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
