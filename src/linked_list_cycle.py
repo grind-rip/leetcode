f105ca4cd20fcffd90cfc7d49c3891b030a0f89a
nickolashkraus <0x@nickolaskraus.io>
2025-01-15 14:30:34 -0600

    Remove Unicode

--- src/linked_list_cycle.py
+++ src/linked_list_cycle.py
@@ -28,7 +28,7 @@ class Solution:
             return False
 
         # The algorithm maintains two pointers into the given sequence, one
-        # (the tortoise) at xᵢ, and the other (the hare) at x₂ᵢ. At each step
+        # (the tortoise) at xi, and the other (the hare) at x2i. At each step
         # of the algorithm, it increases i by one, moving the tortoise one step
         # forward and the hare two steps forward in the sequence, and then
         # compares the sequence values at these two pointers. The smallest
